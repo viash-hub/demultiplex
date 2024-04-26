8556df67be4feb9c6dbf60bf11097984a1839437
DriesSchaumont <5946712+DriesSchaumont@users.noreply.github.com>
2024-04-26 13:08:30 +0200

    FEAT: add untar component

---
+++ src/io/untar/script.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+extra_args=()
+
+
+# Check if tarball contains 1 top-level directory. If so, extract the contents of the
+# directory to the output directory instead of the directory itself.
+echo "Directory contents:"
+tar -taf "${par_input}" 
+
+printf "Checking if tarball contains only a single top-level directory: "
+if [[ $(tar -taf ${par_input} | grep -o -E "^.*?\\/" | uniq | wc -l) -eq 1 ]]; then
+    echo "It does."
+    echo "Extracting the contents of the top-level directory to the output directory instead of the directory itself."
+    extra_args+=("--strip-components=1")
+else
+    echo "It does not."
+fi
+
+if [ "$par_exclude" != "" ]; then
+    echo "Exclusion of files with wildcard '$par_exclude' requested."
+    extra_args+=("--exclude=$par_exclude")
+fi
+
+echo "Starting extraction of tarball '$par_input' to output directory '$par_output'."
+mkdir -p "$par_output"
+tar --directory="$par_output" ${extra_args[@]} -xavf "$par_input"
+
