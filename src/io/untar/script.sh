bba9bc85a0226d4173cac9249c10fe3d66dbded0
Dries Schaumont <5946712+DriesSchaumont@users.noreply.github.com>
2024-04-30 15:34:49 +0200

    BUG: make 'untar' work when extracting a single directory starting with './' (#6)

--- src/io/untar/script.sh
+++ src/io/untar/script.sh
@@ -4,17 +4,27 @@ set -eo pipefail
 
 extra_args=()
 
+TMPDIR=$(mktemp -d "$meta_temp_dir/$meta_functionality_name-XXXXXX")
+function clean_up {
+  [[ -d "$TMPDIR" ]] && rm -r "$TMPDIR"
+}
+trap clean_up EXIT
 
 # Check if tarball contains 1 top-level directory. If so, extract the contents of the
 # directory to the output directory instead of the directory itself.
 echo "Directory contents:"
-tar -taf "${par_input}" 
+tar -taf "${par_input}" > "$TMPDIR/tar_contents.txt"
+cat "$TMPDIR/tar_contents.txt"
 
 printf "Checking if tarball contains only a single top-level directory: "
-if [[ $(tar -taf ${par_input} | grep -o -E "^.*?\\/" | uniq | wc -l) -eq 1 ]]; then
+if [[ $(grep -o -E "^.*?\\/" "$TMPDIR/tar_contents.txt" | uniq | wc -l) -eq 1 ]]; then
     echo "It does."
     echo "Extracting the contents of the top-level directory to the output directory instead of the directory itself."
-    extra_args+=("--strip-components=1")
+    # The directory can be both of the format './<directory>' (or ././<directory>) or just <directory>
+    # Adjust the number of stripped components accordingly by looking for './' at the beginning of the file.
+    starting_relative=$(grep -oP -m 1 '^(./)*' "$TMPDIR/tar_contents.txt" | tr -d '\n' | wc -c)
+    n_strips=$(( ($starting_relative / 2)+1 ))
+    extra_args+=("--strip-components=$n_strips")
 else
     echo "It does not."
 fi
@@ -26,5 +36,6 @@ fi
 
 echo "Starting extraction of tarball '$par_input' to output directory '$par_output'."
 mkdir -p "$par_output"
+echo "executing 'tar --directory=$par_output ${extra_args[@]} -xavf $par_input'"
 tar --directory="$par_output" ${extra_args[@]} -xavf "$par_input"
 
