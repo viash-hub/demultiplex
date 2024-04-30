8bd6ff8709b6ff248f033270976de6b9446f75c0
DriesSchaumont <5946712+DriesSchaumont@users.noreply.github.com>
2024-04-30 09:43:16 +0200

    Remove debugging output from tar component

--- src/io/untar/script.sh
+++ src/io/untar/script.sh
@@ -6,12 +6,8 @@ extra_args=()
 
 # Check if tarball contains 1 top-level directory. If so, extract the contents of the
 # directory to the output directory instead of the directory itself.
-echo "Directory contents:"
-tar -taf "${par_input}" > tar_contents.txt
-cat tar_contents.txt
-
 printf "Checking if tarball contains only a single top-level directory: "
-if [[ $(cat tar_contents | grep -o -E "^[./]*[^/]+/$" | uniq | wc -l) -eq 1 ]]; then
+if [[ $(tar -taf ${par_input} | grep -o -E "^[./]*[^/]+/$" | uniq | wc -l) -eq 1 ]]; then
     echo "It does."
     echo "Extracting the contents of the top-level directory to the output directory instead of the directory itself."
     extra_args+=("--strip-components=1")
