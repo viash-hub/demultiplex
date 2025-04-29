baae584d7aff89c0ddb3f40f5e2cadd00d9b03dc
Dries Schaumont <5946712+DriesSchaumont@users.noreply.github.com>
2025-04-29 14:04:00 +0000

    Add tests for interop_summary_to_csv

---
+++ src/io/interop_summary_to_csv/test.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+# create tempdir
+echo ">>> Creating temporary test directory."
+TMPDIR=$(mktemp -d "$meta_temp_dir/$meta_functionality_name-XXXXXX")
+function clean_up {
+  [[ -d "$TMPDIR" ]] && rm -r "$TMPDIR"
+}
+trap clean_up EXIT
+echo ">>> Created temporary directory '$TMPDIR'."
+
+echo ">>> Run simple execution"
+./$meta_functionality_name \
+   --input "$meta_resources_dir/iseq-DI" \
+   --output_run_summary "$TMPDIR/run_summary.csv" \
+   --output_index_summary "$TMPDIR/index_summary.csv"
